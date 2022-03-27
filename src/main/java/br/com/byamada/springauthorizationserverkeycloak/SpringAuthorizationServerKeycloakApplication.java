e3a92934f98377d2dc26c6d1268daf991cbbdace
Bruno Yamada <brunofernandoyamada@gmail.com>
2022-03-27 18:18:38 -0300

    Adding docker-compose and instructions to configure keycloak users, roles, clients, etc.

---
+++ src/main/java/br/com/byamada/springauthorizationserverkeycloak/SpringAuthorizationServerKeycloakApplication.java
@@ -0,0 +1,13 @@
+package br.com.byamada.springauthorizationserverkeycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAuthorizationServerKeycloakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAuthorizationServerKeycloakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/byamada/springauthorizationserverkeycloak/SpringAuthorizationServerKeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.byamada.springauthorizationserverkeycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuthorizationServerKeycloakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
